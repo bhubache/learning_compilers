a25bc42a87a4bda30c2cb50400a1fdf96419da02
bhubache <brandon.hubacher@gmail.com>
2026-03-24 18:01:36 -0500

    Get initial parser working

--- src/Main.hs
+++ src/Main.hs
@@ -1,20 +1,50 @@
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
-import Main.Utf8 qualified as Utf8
+import Options.Generic
+import Parse qualified
+import System.Directory
+import System.Exit
+import System.FilePath
+import System.Process
 
-data Example = Example
-  { name :: Text
-  , age :: Int
-  }
-  deriving stock (Show, Eq)
+data Labeled = Example {lex :: Bool, parse :: Bool, codegen :: Bool}
+  deriving (Generic, Show)
 
-{- |
- Main entry point.
+instance ParseRecord Labeled
 
- `just run` will invoke this function.
--}
-main :: IO ()
+newtype Unlabeled = Unlabeled FilePath deriving (Generic, Show)
+
+instance ParseRecord Unlabeled
+
+data Mixed = Mixed Labeled Unlabeled deriving (Show)
+
+instance ParseRecord Mixed where
+  parseRecord = Mixed <$> parseRecord <*> parseRecord
+
+-- TODO: Add -S option to emit assembly file but not assemble or link it
+
+getFile :: Mixed -> FilePath
+getFile (Mixed _ (Unlabeled filePath)) = filePath
+
+main :: IO (String, Parse.Program)
 main = do
-  -- For withUtf8, see https://serokell.io/blog/haskell-with-utf8
-  Utf8.withUtf8 $ do
-    putTextLn "Hello 🌎 (from learning-compilers)"
+  record <- getRecord "Learning compilers and haskell"
+
+  let inputFilePath = getFile record
+  let baseFileName = takeBaseName inputFilePath
+  let preprocessedFilePath = takeDirectory inputFilePath </> (baseFileName ++ ".i")
+
+  _ <- readProcess "gcc" ["-E", "-P", inputFilePath, "-o", preprocessedFilePath] ""
+
+  ast <- Parse.parse preprocessedFilePath
+
+  removePathForcibly preprocessedFilePath
+
+  putStr (show ast)
+
+  maybe exitFailure return ast
+
+-- TODO: pass to existing assembler
---
+++ src/Parse.hs
@@ -0,0 +1,141 @@
+module Parse where
+
+import Control.Applicative
+import Data.Char
+import System.IO as SysIO
+
+parse :: FilePath -> IO (Maybe (String, Program))
+parse sourceFile = do
+  content <- SysIO.readFile sourceFile
+  return (parseText content)
+
+parseText :: String -> Maybe (String, Program)
+parseText content =
+  let
+    ast = runParser program content
+   in
+    case ast of
+      Just ([], _) -> ast
+      Just (rest, _)
+        | all isSpace rest -> ast
+        | otherwise -> Nothing
+      _ -> Nothing
+
+newtype Program = Program Function
+  deriving (Show)
+
+data Function = Function ReturnType Identifier Params Body
+  deriving (Show)
+
+newtype ReturnType = ReturnType String
+  deriving (Show)
+
+newtype Identifier = Identifier String
+  deriving (Show)
+
+data Params
+  = ParamList [Param]
+  | Void
+  deriving (Show)
+
+data Param = Param CType Identifier
+  deriving (Show)
+
+newtype CType = CType String
+  deriving (Show)
+
+newtype Body = ReturnStatement Expression
+  deriving (Show)
+
+data Expression
+  = WrappedExpression String
+  | Constant Integer
+  deriving (Show)
+
+program :: Parser Program
+program = Program <$> function
+
+function :: Parser Function
+function = Function <$> returnType <*> identifier <*> paramList <*> body
+
+ctype :: Parser CType
+ctype = CType <$> stringP "int"
+
+identifier :: Parser Identifier
+identifier = Identifier <$> spanP isAlpha
+
+constant :: Parser Expression
+constant = f <$> spanP isDigit
+  where
+    f digits = Constant $ read digits
+
+-- TODO: Reuse `identifier`
+returnType :: Parser ReturnType
+returnType = ReturnType <$> (whitespace *> stringP "int" <* whitespace)
+
+paramList :: Parser Params
+paramList = Void <$ (whitespace *> charP '(' *> whitespace *> stringP "void" <* whitespace <* charP ')') <* whitespace
+
+param :: Parser Param
+param = undefined
+
+body :: Parser Body
+body = ReturnStatement <$> (whitespace *> charP '{' *> whitespace *> line <* whitespace <* charP '}')
+  where
+    line = stringP "return" *> whitespace *> expression <* whitespace <* charP ';'
+
+expression :: Parser Expression
+expression = Parser $ \input ->
+  case runParser constant input of
+    Just (rest, Constant output) -> Just (rest, Constant output)
+    Nothing ->
+      case runParser identifier input of
+        Just (rest, Identifier output) -> Just (rest, WrappedExpression output)
+        Nothing -> Nothing
+
+whitespace :: Parser String
+whitespace = spanP isSpace
+
+charP :: Char -> Parser Char
+charP c = Parser f
+  where
+    f (x : xs)
+      | x == c = Just (xs, c)
+      | otherwise = Nothing
+    f [] = Nothing
+
+stringP :: String -> Parser String
+stringP = traverse charP
+
+spanP :: (Char -> Bool) -> Parser String
+spanP f = Parser $ \input ->
+  let
+    (token, rest) = span f input
+   in
+    Just (rest, token)
+
+newtype Parser a = Parser
+  { runParser :: String -> Maybe (String, a)
+  }
+
+instance Functor Parser where
+  fmap f (Parser p) = Parser $ \input ->
+    case p input of
+      Just (rest, output) -> Just (rest, f output)
+      _ -> Nothing
+
+instance Applicative Parser where
+  pure a = Parser $ \input -> Just (input, a)
+
+  Parser p1 <*> Parser p2 = Parser $ \input ->
+    case p1 input of
+      Nothing -> Nothing
+      Just (rest, f) ->
+        case p2 rest of
+          Nothing -> Nothing
+          Just (rest', output) -> Just (rest', f output)
+
+instance Alternative Parser where
+  empty = Parser $ const Nothing
+
+  Parser p1 <|> Parser p2 = Parser $ \input -> p1 input <|> p2 input
